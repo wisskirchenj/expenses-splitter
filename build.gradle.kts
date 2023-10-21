b63d76976de2b3cde932437b6f354414d4e76cc2
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-10-21 16:30:28 +0200

    bump gradle, spring, bind sonarqube in container

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.1.3"
+    id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.sonarqube") version "4.2.1.3168"
+    //id("org.sonarqube") version "4.3.1.3277"
+    jacoco
 }
 
 group = "de.cofinpro"
@@ -11,6 +14,28 @@ configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "expenses-splitter")
+        property("sonar.projectName", "expenses-splitter")
+        property("sonar.jacoco.reportPaths", "build/reports/jacoco")
+        property("sonar.junit.reportPaths", "build/test-results/test")
+        property("sonar.token", "sqp_d436210e94100d7ef46cf7a5c9ffabcafdd12657")
+        property("sonar.host.url", "http://localhost:9000")
+    }
+}
+
+tasks.sonar {
+    dependsOn(tasks.jacocoTestReport)
 }
 
 repositories {
